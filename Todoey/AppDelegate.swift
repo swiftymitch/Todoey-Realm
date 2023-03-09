35cd9bc0bf2263e9bcccf5398aa7bd09870d574e
mitchxxmitch-code <127380004+swiftymitch@users.noreply.github.com>
2023-03-09 08:55:20 +0100

    Initial commit

---
+++ Todoey/AppDelegate.swift
@@ -0,0 +1,33 @@
+
+import UIKit
+import RealmSwift
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        // print(Realm.Configuration.defaultConfiguration.fileURL)
+        
+        do {
+            let realm = try Realm()
+        } catch {
+            print("Error initialising new realm, \(error)")
+        }
+        
+        
+        
+        return true
+    }
+    
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        
+    }
+
+    
+    
+    
+}
